b248ed3aa0d613fe1af4bd6ac4ee2fd5bb242e14
nisola shobayo <pshobayo@gmail.com>
2015-12-29 19:02:36 -0500

    Update multiclient.py

--- multiclient.py
+++ multiclient.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python                                                                                                                                                                                          
-# This is the multi-threaded client.  This program should be able to run
-# with no arguments and should connect to "127.0.0.1" on port 8765.  It
-# should run a total of 1000 operations, and be extremely likely to
-# encounter all error conditions described in the README.
                                                                                                                                                    
 import sys
 import socket
